449f1d9f0db363987222c2438d22a3fa7323def7
Olexander <olexander.sj@gmail.com>
2019-01-03 23:21:35 +0200

    Adding input search and friend select click event

--- components/Data.js
+++ components/Data.js
@@ -1,7 +1,7 @@
 let friends = [];
 
 const getFriends = async () => {
-    const NUMBER_OF_FRIENDS = 42;
+    const NUMBER_OF_FRIENDS = 35;
 	const FRIENDS_API_LINK = `https://randomuser.me/api/?results=${NUMBER_OF_FRIENDS}`;
 	try {
 		const response = await fetch(FRIENDS_API_LINK);
@@ -16,12 +16,13 @@ const getFriends = async () => {
 }
 
 class Friend {
-    constructor(photo, name, age, phone) {
+    constructor(photo, name, age, phone, visible = true) {
         this.id = Friend.id();
         this.photo = photo;
         this.name = name;
         this.age = age;
         this.phone = phone;
+        this.visible = visible;
     }
 
     static id() {
--- components/Friends.js
+++ components/Friends.js
@@ -5,7 +5,6 @@ const generateFriendsHtml = () => {
     const items_in_row = 7;
     let margin_left = 0;
     let margin_top = -180;
-    console.log(FRIENDS_LIST);
     let html_string = '<div class="friends-list">';
     for (let i = 0; i < FRIENDS_LIST.length; i++) {
       if (i % items_in_row === 0) {
@@ -16,17 +15,30 @@ const generateFriendsHtml = () => {
       html_string += `<div class="friend" id="${FRIENDS_LIST[i].id}" style="margin-left: ${margin_left}px; margin-top: ${margin_top}px">
                          <div class="photo" style="background-image: url('${FRIENDS_LIST[i].photo}')"></div>
                          <div class="name">${FRIENDS_LIST[i].name}</div>
-                         <div class="age">${FRIENDS_LIST[i].age}</div>
-                         <div class="phone">${FRIENDS_LIST[i].phone}</div>
+                         <div class="age">Age: ${FRIENDS_LIST[i].age}</div>
+                         <div class="phone">Phone: ${FRIENDS_LIST[i].phone}</div>
                       </div>`;
     }
     return html_string + '</div>';
   }
 
+  const addFriendEvent = () => {
+    const friends = document.getElementsByClassName('friends-list')[0];
+    friends.onclick = (e) => {
+      const friend = e.target.parentNode;
+      const name_age_phone = document.querySelectorAll(`[id='${friend.id}'] > div:nth-last-of-type(-n+3)`);
+      friend.classList.toggle('friend-selected');
+      name_age_phone[0].classList.toggle('name-selected');
+      name_age_phone[1].classList.toggle('age-phone-visible');
+      name_age_phone[2].classList.toggle('age-phone-visible');
+    }
+}
+
 const render = () => {
     const FRIENDS_LIST = generateFriendsHtml()
     const container = document.getElementById('container');
     container.insertAdjacentHTML('beforeend', FRIENDS_LIST);
+    addFriendEvent();
 }
 
 export default render;
