f01028353b2987f15875fb5f95da01a35dd9129c
Olexander <olexander.sj@gmail.com>
2019-01-03 12:25:24 +0200

    fixing link to components

--- components/Filters.js
+++ components/Filters.js
@@ -39,10 +39,20 @@ const sortAge = () => {
 }
 
 const updateDom = () => {
-    const friends = document.querySelectorAll('.friend');
-    friends.forEach( friend => {
-        console.log(friend);
-    });
+    const items_in_row = 7;
+    let margin_left = 0;
+    let margin_top = -180;
+    for (let i = 0; i < friendsData.length; i++) {
+        if (i % items_in_row === 0) {
+          margin_top += 180;
+          margin_left = 0;
+        }
+        else margin_left += 150;
+        const friend_node = document.getElementById(friendsData[i].id);
+        friend_node.style.marginLeft = `${margin_left}px`;
+        friend_node.style.marginTop = `${margin_top}px`;
+        console.log(friend_node);
+    }
 }
 
 const render = () => {
